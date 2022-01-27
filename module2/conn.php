5501ef8c07da24d8d199842f7ef3a2d44cad120d
Shujie <shujie001202@gmail.com>
2022-01-28 01:09:59 +0800

    revert to yuan ben eh conn

--- module2/conn.php
+++ module2/conn.php
@@ -1,13 +1,7 @@
 <?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db)
-
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ump_mykids";
+$conn = new mysqli($servername, $username, $password, $dbname);
 ?>
