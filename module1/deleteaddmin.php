cdf2fddfd04268446645764fe7f16dd33dd490c2
Shujie <shujie001202@gmail.com>
2022-01-28 00:09:12 +0800

    update database connection to deployed database

--- module1/deleteaddmin.php
+++ module1/deleteaddmin.php
@@ -4,8 +4,16 @@
 $adminID = $_POST['adminID'];
 
 
-$link = mysqli_connect('localhost', 'root', '','ump_mykids');
-
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
    
 $query = "DELETE FROM `admin` WHERE adminID = $adminID"	;
 
