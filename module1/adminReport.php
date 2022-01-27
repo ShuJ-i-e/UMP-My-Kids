b37a690c51afc03564158a442f4467eb77760273
Shujie <shujie001202@gmail.com>
2022-01-28 00:16:48 +0800

    update database connection to deployed database

--- module1/adminReport.php
+++ module1/adminReport.php
@@ -144,8 +144,16 @@
                         <th>Year Register</th>
                     </tr>
                     <?php
-                    $link = mysqli_connect('localhost', 'root', '','ump_mykids');
-                    if ($link->connect_error) {
+                    //Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);                    if ($link->connect_error) {
                         die("Connection failed: " . $link->connect_error);
                     } else {
                         $sql = "SELECT `adminID`, `username`, `phoneNumber`, `address`, `yearRegister` from admin";
--- module1/deleteParent.php
+++ module1/deleteParent.php
@@ -9,8 +9,16 @@ if(isset($_POST['parentID'])){
 }
 
 
-$link = mysqli_connect('localhost', 'root', '','ump_mykids');
-
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
    
 $query = "DELETE FROM `parents` WHERE parentID = $parentID"	;
 
