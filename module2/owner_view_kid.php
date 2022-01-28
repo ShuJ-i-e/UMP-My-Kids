527d7706321baf0d7ca30052c306a84cb0f528d3
laggerbomb <prolohzh@gmail.com>
2022-01-28 11:33:24 +0800

    update title from KidKinder to UMP MY KIDS

--- module2/owner_view_kid.php
+++ module2/owner_view_kid.php
@@ -17,7 +17,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>KidKinder - Kindergarten Website Template</title>
+    <title>UMP MY KIDS</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
